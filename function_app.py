e13fbe767bbe813f30b888cb6f7b93c1532682a8
Matthew（ぶちさん） <dmasubuchi@live.jp>
2025-02-26 14:19:18 +0900

    Today is 2025.02.26

--- function_app.py
+++ function_app.py
@@ -22,6 +22,6 @@ def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully2. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Today is 2025.02.26.... Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
